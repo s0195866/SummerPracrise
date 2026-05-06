22a2db2330e992ef3ec59d7a04ba94135be2da78
Daniil <volkodot@mail.ru>
2026-05-06 19:02:22 +0300

    add config  + db connect

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,5 +1,10 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase
+from config import settings
 
+
+engine = create_async_engine(settings.db_url)
+
+async_session = async_sessionmaker(engine)
 
 
